d2b695d6d13417d5f7be2716a3de45911717fc86
Getnet Bogale <125811912+getnetbogale145@users.noreply.github.com>
2024-07-04 17:21:20 +0300

    commit

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
-import Question from '../../Components/Question/Question'
+// import React from 'react'
+// import Question from '../../Components/Question/Question'
+
+// function Home() {
+//   return (
+//         <>
+//             <Question />
+//         </>
+//   )
+// }
+
+// export default Home
+
+import React, { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AppState } from "../App";
 
 function Home() {
+  const { user, setUser } = useContext(AppState);
+  const navigate = useNavigate();
+
+  function logout() {
+    localStorage.removeItem("token");
+    setUser({});
+    navigate("/login");
+  }
+
   return (
-        <>
-            <Question />
-        </>
-  )
+    <div>
+      <h2>Welcome, {user.username}</h2>
+      {/* Logout Button */}
+      <button onClick={logout}>Logout</button>
+
+      {/* Navigation Links */}
+      <ul>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+        <li>
+          <Link to="/settings">Settings</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
