1815585d605d0b6db0aa696160388e87c4611b7e
Getnet Bogale <125811912+getnetbogale145@users.noreply.github.com>
2024-07-04 16:35:22 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import "./App.css";
+// import "./App.css";
+// import Gateway from "./Router";
+
+// function App() {
+//   return (
+//     <>
+//       <Gateway />
+//     </>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import Gateway from "./Router";
+import "./App.css";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Gateway />
-    </>
+    </BrowserRouter>
   );
 }
 
