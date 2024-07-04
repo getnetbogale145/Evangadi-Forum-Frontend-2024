c2fff600bae9ad0978a6c06ede570d1a107de2b3
Getnet Bogale <125811912+getnetbogale145@users.noreply.github.com>
2024-07-04 18:18:17 +0300

    commit

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,77 +1,15 @@
-// import React, { createContext, useEffect, useState } from "react";
-
-// import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
-// import Landing from "./Pages/Landing/Landing";
-// import axios from "./Utility/axios";
-// import Home from "./Pages/Home/Home";
-// import AskQUestion from "./Components/AskQuestion/AskQUestion";
-// import Detail from "./Components/Details/Detail";
-// import { AnimatePresence } from "framer-motion";
-// import NotFound from "./Pages/NotFound/NotFound";
-// import Updatepass from "./Components/Authentication/UpdatePass";
-
-
-// export const AppState = createContext();
-
-// function Gateway() {
-//   const [user, setUser] = useState({});
-//   const navigate = useNavigate();
-//   const token = localStorage.getItem("token");
-//   const location = useLocation();
-//   // console.log(token)
-
-//   async function checkUserValidity() {
-//     try {
-//       const { data } = await axios.get(`/users/check`, {
-//         headers: {
-//           Authorization: "Bearer " + token,
-//         },
-//       });
-
-//       // console.log(data)
-//       setUser(data);
-//     } catch (error) {
-//       console.log(error.response.data);
-//       setUser(error.response.data);
-//       navigate("/");
-//     }
-//   }
-
-//   useEffect(() => {
-//     checkUserValidity();
-//   }, []);
-
-//   return (
-//     <AppState.Provider value={{ user, setUser }}>
-//       <AnimatePresence>
-//         <Routes location={location} key={location.pathname}>
-//           <Route path="/" element={<Landing />} />
-//           <Route path="/home" element={<Home />} />
-//           <Route path="/question" element={<AskQUestion />} />
-//           <Route path="/home/:answerdetail" element={<Detail />} />
-//           <Route path="*" element={<NotFound />} />
-//         </Routes>
-//       </AnimatePresence>
-//     </AppState.Provider>
-//   );
-// }
-
-// export default Gateway;
-
-
 import React, { createContext, useEffect, useState } from "react";
+
 import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import Landing from "./Pages/Landing/Landing";
 import axios from "./Utility/axios";
 import Home from "./Pages/Home/Home";
-import AskQuestion from "./Components/AskQuestion/AskQuestion";
+import AskQUestion from "./Components/AskQuestion/AskQUestion";
 import Detail from "./Components/Details/Detail";
 import { AnimatePresence } from "framer-motion";
 import NotFound from "./Pages/NotFound/NotFound";
-import UpdatePass from "./Components/Authentication/UpdatePass";
-import SignIn from "./Components/Authentication/SignIn";
-import SignUp from "./Components/Authentication/SignUp";
-import Reset from "./Components/Authentication/Reset";
+import Updatepass from "./Components/Authentication/UpdatePass";
+
 
 export const AppState = createContext();
 
@@ -80,6 +18,7 @@ function Gateway() {
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
   const location = useLocation();
+  // console.log(token)
 
   async function checkUserValidity() {
     try {
@@ -89,6 +28,7 @@ function Gateway() {
         },
       });
 
+      // console.log(data)
       setUser(data);
     } catch (error) {
       console.log(error.response.data);
@@ -98,12 +38,8 @@ function Gateway() {
   }
 
   useEffect(() => {
-    if (token) {
-      checkUserValidity();
-    } else {
-      navigate("/");
-    }
-  }, [token]);
+    checkUserValidity();
+  }, []);
 
   return (
     <AppState.Provider value={{ user, setUser }}>
@@ -111,12 +47,8 @@ function Gateway() {
         <Routes location={location} key={location.pathname}>
           <Route path="/" element={<Landing />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/question" element={<AskQuestion />} />
+          <Route path="/question" element={<AskQUestion />} />
           <Route path="/home/:answerdetail" element={<Detail />} />
-          <Route path="/login" element={<SignIn />} />
-          <Route path="/register" element={<SignUp />} />
-          <Route path="/forgot-password" element={<Reset />} />
-          <Route path="/reset/update" element={<UpdatePass />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </AnimatePresence>
@@ -125,3 +57,4 @@ function Gateway() {
 }
 
 export default Gateway;
+
