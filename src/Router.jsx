c82f2a4a57d396700fbe091ba67690b8f302039f
Getnet Bogale <125811912+getnetbogale145@users.noreply.github.com>
2024-07-04 16:40:27 +0300

    commit

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,15 +1,74 @@
-import React, { createContext, useEffect, useState } from "react";
+// import React, { createContext, useEffect, useState } from "react";
 
+// import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
+// import Landing from "./Pages/Landing/Landing";
+// import axios from "./Utility/axios";
+// import Home from "./Pages/Home/Home";
+// import AskQUestion from "./Components/AskQuestion/AskQUestion";
+// import Detail from "./Components/Details/Detail";
+// import { AnimatePresence } from "framer-motion";
+// import NotFound from "./Pages/NotFound/NotFound";
+// import Updatepass from "./Components/Authentication/UpdatePass";
+
+
+// export const AppState = createContext();
+
+// function Gateway() {
+//   const [user, setUser] = useState({});
+//   const navigate = useNavigate();
+//   const token = localStorage.getItem("token");
+//   const location = useLocation();
+//   // console.log(token)
+
+//   async function checkUserValidity() {
+//     try {
+//       const { data } = await axios.get(`/users/check`, {
+//         headers: {
+//           Authorization: "Bearer " + token,
+//         },
+//       });
+
+//       // console.log(data)
+//       setUser(data);
+//     } catch (error) {
+//       console.log(error.response.data);
+//       setUser(error.response.data);
+//       navigate("/");
+//     }
+//   }
+
+//   useEffect(() => {
+//     checkUserValidity();
+//   }, []);
+
+//   return (
+//     <AppState.Provider value={{ user, setUser }}>
+//       <AnimatePresence>
+//         <Routes location={location} key={location.pathname}>
+//           <Route path="/" element={<Landing />} />
+//           <Route path="/home" element={<Home />} />
+//           <Route path="/question" element={<AskQUestion />} />
+//           <Route path="/home/:answerdetail" element={<Detail />} />
+//           <Route path="*" element={<NotFound />} />
+//         </Routes>
+//       </AnimatePresence>
+//     </AppState.Provider>
+//   );
+// }
+
+// export default Gateway;
+
+
+import React, { createContext, useEffect, useState } from "react";
 import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import Landing from "./Pages/Landing/Landing";
 import axios from "./Utility/axios";
 import Home from "./Pages/Home/Home";
-import AskQUestion from "./Components/AskQuestion/AskQUestion";
+import AskQuestion from "./Components/AskQuestion/AskQuestion";
 import Detail from "./Components/Details/Detail";
 import { AnimatePresence } from "framer-motion";
 import NotFound from "./Pages/NotFound/NotFound";
-import Updatepass from "./Components/Authentication/UpdatePass";
-
+import UpdatePass from "./Components/Authentication/UpdatePass";
 
 export const AppState = createContext();
 
@@ -18,7 +77,6 @@ function Gateway() {
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
   const location = useLocation();
-  // console.log(token)
 
   async function checkUserValidity() {
     try {
@@ -28,7 +86,6 @@ function Gateway() {
         },
       });
 
-      // console.log(data)
       setUser(data);
     } catch (error) {
       console.log(error.response.data);
@@ -38,8 +95,12 @@ function Gateway() {
   }
 
   useEffect(() => {
-    checkUserValidity();
-  }, []);
+    if (token) {
+      checkUserValidity();
+    } else {
+      navigate("/");
+    }
+  }, [token]);
 
   return (
     <AppState.Provider value={{ user, setUser }}>
@@ -47,7 +108,7 @@ function Gateway() {
         <Routes location={location} key={location.pathname}>
           <Route path="/" element={<Landing />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/question" element={<AskQUestion />} />
+          <Route path="/question" element={<AskQuestion />} />
           <Route path="/home/:answerdetail" element={<Detail />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
