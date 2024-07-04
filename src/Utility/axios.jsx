95b7315d5803151fec842d1690c3244f970cde9f
Getnet Bogale <125811912+getnetbogale145@users.noreply.github.com>
2024-07-04 15:12:33 +0300

    commit

--- src/Utility/axios.jsx
+++ src/Utility/axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "https://github.com/getnetbogale145/Evangadi-Forum-Backend-2024",
-  baseURL: "https://evanforum-backend-2.onrender.com/api",
+  baseURL: "https://evangadi-forum-backend-2024-5.onrender.com",
 });
 
 export default instance;
